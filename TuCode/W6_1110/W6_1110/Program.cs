eed54a533286017560c28a26bd33da82a2c18c50
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2022-12-07 23:26:01 +0700

    w6_1110 initial

---
+++ TuCode/W6_1110/W6_1110/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W6_1110
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ TuCode/W6_1110/W6_1110/Subject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace W6_1110
+{
+    public class Subject
+    {
+        protected List<Observer> subcribers = new List<Observer>();
+        //Subcribe any observer and return success/fail subcribe
+        public bool Subcribe(Observer o)
+        {
+            subcribers.Add(o);
+            return true;
+        }
+    }
+}
